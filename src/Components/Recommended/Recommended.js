c6505696aeeb89d1691155c07c3565318fbd066b
Akhil-5292 <aps.ca9@gmail.com>
2023-05-17 12:18:06 +0530

    UI

--- src/Components/Recommended/Recommended.js
+++ src/Components/Recommended/Recommended.js
@@ -11,8 +11,10 @@ function Recommended() {
     <h2>Recommended</h2>
       <ShowToggle
         title="Recommended for you"
-        imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkIfDPcmuFr_XvXNuxvENzzFNR9kS2F9iKLw&usqp=CAU"
-        description="Description for recommended shows"
+        imageUrl="https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-03.jpg"
+        description="Avengers: Age of Ultron the Action/Sci-fi movie is a 2015 superhero film, based on the Marvel Comics superhero team of the same name. 
+        The film is a sequel to The Avengers, Iron Man 3, Thor: The Dark World, and Captain America: The Winter Soldier. It is the eleventh film in the Marvel Cinematic Universe,
+         and the fifth installment of Phase Two. The film was released on April 22, 2015 internationally and on May 1, 2015 in the United States."
       />
       </div>
     </div>
